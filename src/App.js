77f06704132e9c41a8d9e493e6cc77c844f07ba4
Abhay Jirati <abhi.jrt12@gmail.com>
2020-08-08 14:21:47 +0530

    adding the create name with tooltip

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  function toolTipForCreater(e) {
+    console.log(e);
+    console.log("Target", e.target);
+    console.log("Style", e.target.style);
+  }
+
+  function removeToolTip(e) {
+    e.target.style.backgroundColor = "";
+  }
+
   return (
-    <div className="App">
-      <h1>You are Good to Go</h1>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>Tool-Tip</h1>
+        <span className="toolTipCreaterForHeading">Design By Abhay Jirati</span>
+      </div>
+      <div>
+        <button>Download</button>
+        <span>50 MB File Size</span>
+      </div>
     </div>
   );
 }
